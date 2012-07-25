f460c59f92319777a88c9085711160fa7038709c
maxin <ma.xin@micricyber.cn>
2012-07-25 17:15:12 +0800

    add element

--- RailView/RailView/Form1.cs
+++ RailView/RailView/Form1.cs
@@ -14,6 +14,26 @@ namespace RailView
         public Form1()
         {
             InitializeComponent();
+            MyInit();
         }
+
+        private void MyInit()
+        {
+            // 设置Control的相关Style，主要与绘制有关
+            this.SetStyle(ControlStyles.DoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.ContainerControl |
+                ControlStyles.UserPaint | ControlStyles.Selectable | ControlStyles.UserMouse, true);
+
+        }
+
+        private void panel2_DragDrop(object sender, DragEventArgs e)
+        {
+            ;
+        }
+
+        private void panel2_DragEnter(object sender, DragEventArgs e)
+        {
+            ;
+        }
+
     }
 }
