3056f743c1ae8b006a182cf2ddb6463d8efcb370
Sdny <smallsmilejiang@msn.com>
2012-08-14 15:58:39 +0800

    1, 添加MES功能ICE接口文件及编译脚本.

--- src/ice_interface/prefiles.bat
+++ src/ice_interface/prefiles.bat
@@ -1,6 +1,5 @@
-cd ../compile_tools
-slice2cpp ../ice/LogInterface.ice --add-header "stdafx.h" --output-dir ../Logger/common/Win32
-slice2cppe ../ice/LogInterface.ice --add-header "stdafx.h" --output-dir ../Logger/common/CE
-slice2cs ../ice/LogInterface.ice  --output-dir ../Logger/LogWinClientCS
+cd ../ice_tools
+slice2cpp ../ice_interface/MesLink.ice --add-header "stdafx.h" --output-dir ../common
+slice2cs ../ice_interface/MesLink.ice  --output-dir ../common
 
 pause
