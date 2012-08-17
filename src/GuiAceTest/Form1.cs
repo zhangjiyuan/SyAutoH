83182c9194b8e44bde0311338d819b03dc165ee7
Sdny <smallsmilejiang@msn.com>
2012-08-17 11:53:23 +0800

    1, gui test UI

--- src/GuiAceTest/Form1.cs
+++ src/GuiAceTest/Form1.cs
@@ -15,5 +15,30 @@ namespace GuiAceTest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.labelInfo.Text = "Login and do data operate.";
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonTagRead_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonTagWrite_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
