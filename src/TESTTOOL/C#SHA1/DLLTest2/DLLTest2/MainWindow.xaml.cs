981a1ed60032d27085e13678e039ab471fc22006
zhangjiyuan <zhang.jiyuan@microcyber.cn>
2012-09-10 11:55:34 +0800

    添加的SHA1的C#实现，包括DLL文件以及调试

---
+++ src/TESTTOOL/C#SHA1/DLLTest2/DLLTest2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SHA1DLL;
+
+namespace DLLTest2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string input = inputstr.Text;
+            Class1 test1 = new Class1();
+            string output = test1.SHA1Transform(input);
+            outputstr.Text = output;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            string name = inputName.Text;
+            string high = inputHigh.Text;
+            string low = inputLow.Text;
+            string passWord = inputPassWord.Text;
+            Class1 test2 = new Class1();
+            string SHA1Message = test2.HashLoginInfo(name, high, low, passWord);
+            outputMessage.Text = SHA1Message;
+        }
+    }
+}
---
+++ src/TESTTOOL/C#SHA1/SHA1DLL/SHA1DLL/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace SHA1DLL
+{
+    public class Class1
+    {
+        public string SHA1Transform(string inMessage)
+        {
+            string outMessage;
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            ASCIIEncoding enc = new ASCIIEncoding();
+            byte[] dataToHash = enc.GetBytes(inMessage);
+            byte[] dataHash = sha.ComputeHash(dataToHash);
+            outMessage = BitConverter.ToString(dataHash);
+            outMessage = outMessage.Replace("-", "");
+            return outMessage;
+        }
+        public string HashLoginInfo(string sName, string sHighMark, string sLowMark, string sPassword)
+        {
+            string infoInHash;
+            char firstWord;
+            sName = sName.ToLower();
+            firstWord =sName[0];
+            char[] last = new char[4];
+            for(int i=0;i<4;i++)
+            {
+                last[3-i] = (char)(((firstWord>>i)&0x00000001)+'\0');
+            }
+            string middle="";
+            for (int i = 0; i < 4; i++)
+            {
+                if (last[i] == 0)
+                    middle += sLowMark;
+                else
+                    middle += sHighMark;
+            }
+            string messageInfo=sName+middle+sPassword;
+            infoInHash=SHA1Transform(messageInfo);
+            return infoInHash;
+        }
+
+    }
+}
