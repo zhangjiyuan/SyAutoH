08fde7e0e6d843e4e58ce3c9c3535b5d7d9d3afb
zhangjiyuan <zhang.jiyuan@microcyber.cn>
2012-10-23 13:26:15 +0800

    修改添加LOG CONFIG，已无错，利用已存在的singleton common，并做一定修改

---
+++ src/shared/ConfigEnv.h
@@ -0,0 +1,11 @@
+#if !defined(CONFIGENVIRONMENT_H)
+#define CONFIGENVIRONMENT_H
+
+//! Other libs we depend on.
+#include "Common.h"
+
+//! Our own includes.
+#include "Config.h"
+#include "Log.h"
+
+#endif
