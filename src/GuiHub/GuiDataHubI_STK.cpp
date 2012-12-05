be2925b5fb12d002cd86dffc6546afcd116eee70
Sdny <smallsmilejiang@msn.com>
2012-12-05 13:44:03 +0800

    1, missing files

---
+++ src/GuiHub/GuiDataHubI_STK.cpp
@@ -0,0 +1,25 @@
+#include "StdAfx.h"
+#include "GuiDataHubI.h"
+#include "../AMHSDrive/AMHSDrive.h"
+#include "../shared/Util.h"
+#include "../shared/Log.h"
+#include "../SqlAceCli/SqlAceCli.h"
+#include "IceUtil/Unicode.h"
+#include "iConstDef.h"
+
+void GuiDataHubI::STK_SetStatusBackTime(const std::string& strVal)
+{
+	STR_VEC vecStr = GetVecStrings(strVal);
+	for(STR_VEC::iterator it = vecStr.begin();
+		it != vecStr.end(); ++it)
+	{
+		string strE = *it;
+		STR_VEC Params = SplitString(*it, ",");
+		if (Params.size() == 2)
+		{
+			int nID = atoi(Params[0].c_str());
+			int nTime = atoi(Params[1].c_str());
+			m_pAMHSDrive->STKStatusBackTime(nID, nTime);
+		}
+	}
+}
